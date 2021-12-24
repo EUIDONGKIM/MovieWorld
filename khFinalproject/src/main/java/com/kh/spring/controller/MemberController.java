12d4d0b6bc31cb18efe9520a260f1aee6f322549
hongdyun9410 <dkwntbvj@naver.com>
2021-12-24 11:59:12 +0900

    회원가입 및 로그인구현
    -controller
    -mapper 구문추가
    -jsp 페이지 생성

--- khFinalproject/src/main/java/com/kh/spring/controller/MemberController.java
+++ khFinalproject/src/main/java/com/kh/spring/controller/MemberController.java
@@ -1,18 +1,50 @@
 package com.kh.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.spring.entity.MemberDto;
+import com.kh.spring.repository.MemberDao;
 
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
+	@Autowired
+	private MemberDao memberDao;
 	
-
+	//회원가입 
 	@GetMapping("/join")
 	public String join() {
-		return "join";
+		return "member/join";
 	}
+	@PostMapping("/join")
+	public String join(@ModelAttribute MemberDto memberDto) {
+		memberDao.join(memberDto);
+		return "redirect:join_success";
+	}
+	
+	@RequestMapping("/join_success")
+	public String join_success() {
+		return "member/join_success";
+	}
+	
+	//로그인
+	@GetMapping("/login")
+	public String login() {
+		return "member/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(@ModelAttribute MemberDto memberDto) {
+		MemberDto findDto = memberDao.login(memberDto);
+		return "redirect:/";
+	}
+	
+	
 
 }
---
+++ khFinalproject/src/main/webapp/WEB-INF/views/member/join_success.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<jsp:include page="/WEB-INF/views/template/header.jsp"></jsp:include>
+<h1>회원가입 을 환영합니다</h1>
+<a href="#">로그인하러가기</a>
+<jsp:include page="/WEB-INF/views/template/footer.jsp"></jsp:include>
