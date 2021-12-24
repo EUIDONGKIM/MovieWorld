a0cef6a94498014571c7670d7c90b64e46e3f6cb
dolddi6778@naver.com <dolddi6778@naver.com>
2021-12-24 16:03:26 +0900

    상영 시간 수정

--- khFinalproject/src/main/java/com/kh/spring/repository/TotalInfoViewDaoImpl.java
+++ khFinalproject/src/main/java/com/kh/spring/repository/TotalInfoViewDaoImpl.java
@@ -1,6 +1,8 @@
 package com.kh.spring.repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +22,11 @@ public class TotalInfoViewDaoImpl implements TotalInfoViewDao{
 	}
 	//추후에 리스트들을 <where>로 통합.
 	@Override
-	public TotalInfoViewDto get(int hallNo) {
-		return sqlSession.selectOne("totalInfoView.getByHallNo",hallNo);
+	public TotalInfoViewDto get(int hallNo,int movieNo) {
+		Map<String,Object> param = new HashMap<>();
+		param.put("hallNo",hallNo);
+		param.put("movieNo",movieNo);
+		return sqlSession.selectOne("totalInfoView.getByHallMovieNo",param);
 	}
 	@Override
 	public List<TotalInfoViewDto> list(int movieNo) {
