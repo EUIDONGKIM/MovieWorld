2a0b10cb201b3ebaf59f24bbf2d4c64dd405a855
hongdyun9410 <dkwntbvj@naver.com>
2021-12-26 23:44:14 +0900

    비밀번호 찾기 구현 ( 프론트 암호화 안댐)

--- khFinalproject/src/main/java/com/kh/spring/controller/MemberController.java
+++ khFinalproject/src/main/java/com/kh/spring/controller/MemberController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,14 +15,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.spring.entity.MemberDto;
 import com.kh.spring.repository.MemberDao;
+import com.kh.spring.util.RandomUtil;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 @Controller
 @RequestMapping("/member")
+@Slf4j
 public class MemberController {
 	@Autowired
 	private MemberDao memberDao;
+
+	@Autowired
+	private RandomUtil randomUtil;
 	
+
 	//회원가입 
 	@GetMapping("/join")
 	public String join() {
@@ -79,23 +88,57 @@ public class MemberController {
 		return "member/mypage";
 	}
 	
+	//아이디 찾기
 	@GetMapping("/idScan")
 	public String idScan() {
 		return "member/idScan";
 	}
 	@PostMapping("/idScan")
-	public String idScan(@RequestParam String memberEmail) {
+	public String idScan(@RequestParam String memberName,@RequestParam String memberPhone,
+			Model model) {
 		
-		return "ridirect:member/login";
+		MemberDto isPass=memberDao.findId(memberName, memberPhone);
+		if(isPass!=null) {
+			//System.out.println(isPass.getMemberEmail());
+			model.addAttribute("memberEmail",isPass.getMemberEmail());
+			return "member/idScanSuccess";
+		}else {
+			return "redirect:idScan?error";
+		}
+		
 	}
+	@RequestMapping("/idScanSuccess")
+	public String idScanSuccess() {
+		return "member/idScanSuccess";
+	}
+	
 
 	@GetMapping("/pwScan")
 	public String pwScan() {
 		return "member/pwScan";
 	}
 	@PostMapping("/pwScan")
-	public String pwScan(@RequestParam String memberEmail) {
+	public String pwScan(@RequestParam String memberEmail,@RequestParam String memberName,@RequestParam String memberPhone
+			,Model model ,@ModelAttribute MemberDto memberDto) {
+		MemberDto isPass=memberDao.findPw(memberName, memberEmail, memberPhone);
+		//6자리의 랜덤숫자생성
+		String number = randomUtil.generateRandomNumber(6);
 		
-		return "ridirect:member/login";
+		if(isPass!=null) {
+			//6자리의 난수 비밀번호 생성
+			isPass.setMemberPw(number);
+			System.out.println("여기 1");
+
+			String chagePw = isPass.getMemberPw();
+		
+			memberDao.temporayPassword(memberDto,chagePw);
+			System.out.println("여기 2");
+			model.addAttribute("memberPw",chagePw);
+			System.out.println("여기 3");
+			
+			return "member/PwScanSuccess";
+		}else {
+			return "redirect:pwScan?error";
+		}
 	}
 }
