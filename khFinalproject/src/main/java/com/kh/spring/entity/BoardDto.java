f3acfc03f25d50157c29957755de5a5556376b12
hongdyun9410 <dkwntbvj@naver.com>
2021-12-28 10:07:18 +0900

    게시판 DTO 생성

---
+++ khFinalproject/src/main/java/com/kh/spring/entity/BoardDto.java
@@ -0,0 +1,19 @@
+package com.kh.spring.entity;
+
+import java.util.Date;
+
+import lombok.Data;
+@Data
+public class BoardDto {
+	private int boardNo;
+	private int boardTypeNo;
+	private String memberEmail;
+	private String title;
+	private Date boardDate;
+	private String boardContent;
+	private int boardViews;
+	private int boardSuperNo;
+	private int boardGroupNo;
+	private int boardDepth;
+
+}
