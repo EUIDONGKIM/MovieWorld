6966a8893758ed751e69ec3a062b4aa0091a3070
inteyy <kit3413@naver.com>
2021-12-28 14:05:06 +0900

    파일 위치 수정
    board 묶음

--- khFinalproject/src/main/java/com/kh/spring/repository/BoardDao.java
+++
@@ -1,7 +0,0 @@
-package com.kh.spring.repository;
-
-import com.kh.spring.entity.BoardDto;
-
-public interface BoardDao {
-	void write(BoardDto boardDto);
-}
---
+++ khFinalproject/src/main/java/com/kh/spring/repository/board/BoardDao.java
@@ -0,0 +1,7 @@
+package com.kh.spring.repository.board;
+
+import com.kh.spring.entity.board.BoardDto;
+
+public interface BoardDao {
+	void write(BoardDto boardDto);
+}
