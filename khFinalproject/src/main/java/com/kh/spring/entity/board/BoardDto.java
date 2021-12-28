327e097237bc2e109e4c77b8fde671159e1d286f
hongdyun9410 <dkwntbvj@naver.com>
2021-12-28 14:40:24 +0900

    중복 삭제

--- khFinalproject/src/main/java/com/kh/spring/entity/board/BoardDto.java
+++ khFinalproject/src/main/java/com/kh/spring/entity/board/BoardDto.java
@@ -8,12 +8,21 @@ public class BoardDto {
 	private int boardNo;
 	private int boardTypeNo;
 	private String memberEmail;
-	private String title;
+	private String boardTitle;
 	private Date boardDate;
 	private String boardContent;
+	private int boardReply;
 	private int boardViews;
 	private int boardSuperNo;
 	private int boardGroupNo;
 	private int boardDepth;
 
+//	//추가 : 댓글이 존재하는지 확인하는 메소드
+	public boolean isReplyExist() {
+		return this.boardReply > 0;
+	}
+	//추가 : 답변글인지 확인하는 메소드
+	public boolean hasDepth() {
+		return this.boardDepth > 0;
+	}
 }
