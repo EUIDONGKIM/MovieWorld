70fc9270b2dd0ef6dfe2120373a7c5ef8519636a
geonah23 <geona947@gmail.com>
2021-12-31 15:57:29 +0900

    리뷰...는 일단 생성해두었습니다

--- khFinalproject/src/main/java/com/kh/spring/controller/ReviewController.java
+++ khFinalproject/src/main/java/com/kh/spring/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package com.kh.spring.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,5 +10,10 @@ public class ReviewController {
 	@GetMapping("/write")
 	public String write() {
 		return "review/write";
+	}
+	
+	@GetMapping("/list")
+	public String list() {
+		return "movie/list";
 	}
 }
