f6b138a88e3f969485678897aaa26e81f2df8b7d
inteyy <kit3413@naver.com>
2022-01-04 19:37:11 +0900

    왜 삭제됨?

---
+++ khFinalproject/src/main/java/com/kh/spring/vo/MovieChartVO.java
@@ -0,0 +1,15 @@
+package com.kh.spring.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class MovieChartVO {
+	private int movieNo;
+	private String movieTitle;
+	private String movieGrade;
+	private float movieStarpoint;
+	private String movieOpening;
+	private float movieRatio;
+}
