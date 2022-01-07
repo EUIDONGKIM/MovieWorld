6718cc5a1437431996366c709f4b405541aa91ae
inteyy <kit3413@naver.com>
2022-01-07 10:54:54 +0900

    로그아웃하면 멤버번호 세션 삭제

--- khFinalproject/src/main/java/com/kh/spring/controller/MemberController.java
+++ khFinalproject/src/main/java/com/kh/spring/controller/MemberController.java
@@ -129,6 +129,7 @@ public class MemberController {
 	public String logout(HttpSession session) {
 		session.removeAttribute("ses");
 		session.removeAttribute("grade");
+		session.removeAttribute("memberNo");
 		return "redirect:/";
 	}
 	
