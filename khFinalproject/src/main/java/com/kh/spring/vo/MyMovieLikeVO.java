8c5f39222b02b99ee206c35bc7b756238f841d35
inteyy <kit3413@naver.com>
2022-01-07 10:51:35 +0900

    내가 찜한 영화 진행중

---
+++ khFinalproject/src/main/java/com/kh/spring/vo/MyMovieLikeVO.java
@@ -0,0 +1,18 @@
+package com.kh.spring.vo;
+
+import lombok.Data;
+
+@Data
+public class MyMovieLikeVO {
+	private int moviePhotoNo;
+	private int movieNo;
+	private String movieTitle;
+	private String movieEngTitle;
+	private String movieGrade;
+	private String movieType;
+	private String movieCountry;
+	private String movieOpening;
+	private int movieRuntime;
+	private float movieStarpoint;
+	private String movieContent;
+}
